a869ecce28dac680b75f1f5d8ba1a80ed35ad870
matheus amorim <mtxdevfrontend@gmail.com>
2025-05-22 19:15:49 -0300

    Add data manipulation using pandas

---
+++ datamanipulation.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df = pd.read_excel("books.csv.xlsx")
+
+general_avg_stars_df = df["Stars"].mean()
+
+genre_avg_stars_df = df.groupby("Gender").agg(
+    total_stars=("Stars", "sum"),
+    book_count=("Stars", "count"),
+    media_stars=("Stars", "mean")
+)
+
+genre_above_avg_df = genre_avg_stars_df[genre_avg_stars_df["media_stars"] > general_avg_stars_df]
+
+genre_avg_stars_df.to_excel("genre_avg.xlsx", index=False)
+genre_above_avg_df.to_excel("genre_above.xlsx", index=False)
