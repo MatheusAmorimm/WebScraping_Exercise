afcff96dd8dad4cc34b72d4be024d604fac2ac94
matheus amorim <mtxdevfrontend@gmail.com>
2025-06-07 10:17:04 -0300

    add new todo

--- data_manipulation/datamanipulation.py
+++ data_manipulation/datamanipulation.py
@@ -1,16 +1,1 @@
-import pandas as pd
-
-df = pd.read_excel("data/books.csv")
-
-general_avg_stars_df = df["Stars"].mean()
-
-genre_avg_stars_df = df.groupby("Gender").agg(
-    total_stars=("Stars", "sum"),
-    book_count=("Stars", "count"),
-    media_stars=("Stars", "mean")
-).reset_index()
-
-genre_above_avg_df = genre_avg_stars_df[genre_avg_stars_df["media_stars"] > general_avg_stars_df]
-
-genre_avg_stars_df.to_excel("data/genre_avg.xlsx", index=False)
-genre_above_avg_df.to_excel("data/genre_above.xlsx", index=False)
+#TODO: Realizar uma limpeza nos dados e extrair relatorios
